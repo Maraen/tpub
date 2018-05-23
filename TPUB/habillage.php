97af2b4bc540212f4a4ecb9ba1530885b780dfd3
Victor Audétat <33400332+victorvkooz@users.noreply.github.com>
2018-05-23 11:17:14 +0200

    Update habillage.php

--- TPUB/habillage.php
+++ TPUB/habillage.php
@@ -1,3 +1,23 @@
+<?php
+    //CHECK SI VIDE 
+    if (empty($_POST['tramintegral']) or empty($_POST['integralM1']) or empty($_POST['integralLeb']) or empty($_POST['integralBusTrolleybus']) or empty($_POST['arriereBusTrolleyBusandShop']) or empty($_POST['interieurGuerillaMarketing'])) 
+    {
+    	header('location: movingStar.php');
+    }
+    else 
+    {
+	   	$_SESSION['tramintegral'] = isset($_POST['tramintegral']) ? $_POST['tramintegral'] : NULL;
+	    $_SESSION['integralM1'] = isset($_POST['integralM1']) ? $_POST['integralM1'] : NULL;
+	    $_SESSION['integralLeb'] = isset($_POST['integralLeb']) ? $_POST['integralLeb'] : NULL;
+	    $_SESSION['integralBusTrolleybus'] = isset($_POST['integralBusTrolleybus']) ? $_POST['integralBusTrolleybus'] : NULL;
+	   	$_SESSION['arriereBusTrolleyBusandShop'] = isset($_POST['arriereBusTrolleyBusandShop']) ? $_POST['arriereBusTrolleyBusandShop'] : NULL;
+	    $_SESSION['interieurGuerillaMarketing'] = isset($_POST['interieurGuerillaMarketing']) ? $_POST['interieurGuerillaMarketing'] : NULL;
+	    
+	    header('location: step3.php');
+    }
+?>
+
+
 <form>
 	<input type="checkbox" name="tramintegral"> <label> Tram intégral </label> 
 	<input type="checkbox" name="integralM1"> <label> Intégral M1 </label> 
